8945bfdf2d85cc9848bac7dcd764b7794c63a2ef
Carlos Rodríguez <c.rodriguezanton@gmail.com>
2018-10-20 20:22:02 -0700

    Commented tests

--- src/index.js
+++ src/index.js
@@ -18,19 +18,19 @@ function decryptData(data_encrypted, secret){
   return crypto.decryptData(data_encrypted, secret);
 }
 
-//GENERATE KEYPAIR FOR DPO
-dpo_keys = generateKeypair();
+// //GENERATE KEYPAIR FOR DPO
+// dpo_keys = generateKeypair();
 
-//GENERATE KEYPAIR FOR BOARD
-public_board_keys = [];
-private_board_keys = [];
-for (i=0; i<3; i++){
-  public_board_keys.push(generateKeypair()[1]);
-  private_board_keys.push(generateKeypair()[0]);
-}
+// //GENERATE KEYPAIR FOR BOARD
+// public_board_keys = [];
+// private_board_keys = [];
+// for (i=0; i<3; i++){
+//   public_board_keys.push(generateKeypair()[1]);
+//   private_board_keys.push(generateKeypair()[0]);
+// }
 
-//USER WANTS TO DELETE DATA
-response = encryptData("dades del usuari", dpo_keys[1], public_board_keys);
-console.log(response);
+// //USER WANTS TO DELETE DATA
+// response = encryptData("dades del usuari", dpo_keys[1], public_board_keys);
+// console.log(response);
 
 module.exports = { generateKeypair, encryptData, decryptSecrets, decryptDades};
